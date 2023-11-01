e3296e2351904a0b6505ed2420a36f208d467770
annarusak <alitvinetz@gmail.com>
2023-11-01 16:05:37 +0200

    Add timer label, start/pause and lap/reset buttons. Add color and title change after user tap action.

---
+++ TimerApp/extensions/UIButtonExtensions.swift
@@ -0,0 +1,26 @@
+//
+//  Extensions.swift
+//  TimerApp
+//
+//  Created by Анна on 1.11.23.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    
+    func changeAlphaWhenHighlighted() {
+        addTarget(self, action: #selector(buttonHighlightedChanged), for: .touchUpInside)
+        addTarget(self, action: #selector(buttonHighlightedChanged), for: .touchDown)
+        addTarget(self, action: #selector(buttonHighlightedChanged), for: .touchUpOutside)
+    }
+    
+    @objc func buttonHighlightedChanged() {
+        struct Holder {
+            static var isPressed : Bool = false
+        }
+        Holder.isPressed = Holder.isPressed ? false : true
+        alpha = Holder.isPressed ? 0.5 : 0.7
+    }
+}
---
+++ TimerApp/model/ButtonStates.swift
@@ -0,0 +1,20 @@
+//
+//  Enums.swift
+//  TimerApp
+//
+//  Created by Анна on 30.10.23.
+//
+
+import Foundation
+
+    enum StartPauseButtonState: String {
+        case start = "Start"
+        case pause = "Stop"
+    }
+    
+    enum LapResetButtonState: String {
+        case unenabledLap
+        case lap = "Lap"
+        case reset = "Reset"
+    }
+
