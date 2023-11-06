95abd68fbdc89d28581ae3deff2445d7ffc65534
annarusak <alitvinetz@gmail.com>
2023-11-06 13:22:41 +0200

    add temporary lap table view implementation

--- TimerApp/controller/ViewController.swift
+++ TimerApp/controller/ViewController.swift
@@ -16,14 +16,42 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     private let lapResetButtonColor = UIColor(red: 171/255, green: 171/255, blue: 171/255, alpha: 0.4)
     private let buttonDefaultAlpha = 0.7
     private let fractionTimer = FractionTimer()
-    var myTableView = UITableView()
+    var lapTableView = UITableView()
     let identifier = "lapCell"
-    let array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    var lapTimerTimestamps: [String] = []
+    var lastLapsTimeString: [String] = []
+    var lapTimes: [Int] = []
+    var statusOfValue = foundMaxMinValue.middleValue
     
-    var startPauseButtonState = StartPauseButtonState.start
+    enum foundMaxMinValue {
+        case maxValue
+        case minValue
+        case middleValue
+    }
+
+    var lapCount = 0 {
+        didSet {
+            if (lapCount == 0) {
+                lapTimerTimestamps.removeAll()
+            } else {
+                lapTimestampsUpdate()
+            }
+            lapTableView.reloadData()
+        }
+    }
+    
+    var startPauseButtonState = StartPauseButtonState.start {
+        didSet {
+            if (startPauseButtonState == .stop) {
+                fractionTimer.start()
+            } else {
+                fractionTimer.stop()
+            }
+        }
+    }
     var lapResetButtonState = LapResetButtonState.unenabledLap
     
-
+    
     private lazy var timerLabel: UILabel = {
         let label = UILabel()
         label.text = "00:00,00"
@@ -60,6 +88,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         button.changeAlphaWhenHighlighted()
         return button
     }()
+ 
+    func createTable() {
+        //      MARK: - по-другому сделать размерные привязки таблицы
+        lapTableView = UITableView(frame: view.bounds.offsetBy(dx: 0, dy: 550), style: .plain)
+        lapTableView.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
+        lapTableView.delegate = self
+        lapTableView.dataSource = self
+        lapTableView.separatorStyle = .singleLine
+        lapTableView.separatorInset = .init(top: 0, left: 10, bottom: 0, right: 10)
+        lapTableView.separatorColor = .white
+        lapTableView.backgroundColor = .black
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -68,20 +108,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         setupViews()
     }
     
-    func createTable() {
-//      MARK: - по-другому сделать размерные привязки таблицы
-        self.myTableView = UITableView(frame: view.bounds.offsetBy(dx: 0, dy: 550), style: .plain)
-        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
-        self.myTableView.delegate = self
-        self.myTableView.dataSource = self
-        view.addSubview(myTableView)
-    }
-    
     func setupViews() {
         view.backgroundColor = .black
         view.addSubview(lapResetButton)
         view.addSubview(startPauseButton)
         view.addSubview(timerLabel)
+        view.addSubview(lapTableView)
         NSLayoutConstraint.activate([
             lapResetButton.topAnchor.constraint(equalTo: view.centerYAnchor),
             lapResetButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -100),
@@ -93,40 +125,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             startPauseButton.heightAnchor.constraint(equalToConstant: roundButtonSize),
             timerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             timerLabel.bottomAnchor.constraint(equalTo: view.centerYAnchor, constant: -100)
-//            myTableView.topAnchor.constraint(equalTo: startPauseButton.bottomAnchor, constant: -200)
         ])
-    }
-    
-    @objc func lapResetButtonTapped(sender: UIButton) {
-        switch lapResetButtonState {
-        case .reset:
-            timerLabel.text = "00:00,00"
-            fractionTimer.reset()
-            lapResetButtonState = .unenabledLap
-            break
-        case .unenabledLap:
-            return
-        case .lap:
-            timeMeasure()
-            break
-        }
-        setupLapResetButton(button: lapResetButton)
-    }
-    
-    @objc func startPauseButtonTapped(sender: UIButton) {
-        if startPauseButtonState == .start {
-            fractionTimer.start()
-        } else {
-            fractionTimer.stop()
-        }
-        startPauseButtonState = startPauseButtonState == .start ? .stop : .start
-        setupStartPauseButton(button: startPauseButton)
-        lapResetButtonState = (startPauseButtonState == .stop) ? .lap : .reset
-        setupLapResetButton(button: lapResetButton)
-    }
-    
-    private func timeMeasure() {
-        // TODO
     }
     
     func setupStartPauseButton(button: UIButton) {
@@ -159,36 +158,133 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
+    @objc func lapResetButtonTapped(sender: UIButton) {
+        switch lapResetButtonState {
+        case .reset:
+            setupAfterResetButton()
+        case .unenabledLap:
+            return
+        case .lap:
+            lapCount += 1
+        }
+        setupLapResetButton(button: lapResetButton)
+    }
+    
+    @objc func startPauseButtonTapped(sender: UIButton) {
+        startPauseButtonState = startPauseButtonState == .start ? .stop : .start
+        setupStartPauseButton(button: startPauseButton)
+        lapResetButtonState = (startPauseButtonState == .stop) ? .lap : .reset
+        setupLapResetButton(button: lapResetButton)
+    }
+    
+    private func setupAfterResetButton() {
+        timerLabel.text = "00:00,00"
+        fractionTimer.reset()
+        lapResetButtonState = .unenabledLap
+        lapCount = 0
+        lastLapsTimeString.removeAll()
+        lapTimes.removeAll()
+        lapTableView.reloadData()
+        
+    }
+
+    private func lapTimestampsUpdate() {
+        if let timeMeasure = timerLabel.text {
+            lapTimerTimestamps.append(timeMeasure)
+            calculateLapTime()
+        }
+    }
+    
+    private func formatTime(timerPeriod: Int) -> String {
+        let minutes = timerPeriod / 6000  // Calculate minutes (60 seconds * 100 centiseconds)
+        let seconds = (timerPeriod / 100) % 60  // Calculate remaining seconds
+        let centiseconds = timerPeriod % 100  // Calculate remaining centiseconds
+        // Create the formatted string
+        let formattedTime = String(format: "%02d:%02d,%02d", minutes, seconds, centiseconds)
+        
+        return formattedTime
+    }
+    
+    private func calculateLapTime() {
+        if lapCount == 1 {
+            print(lapTimerTimestamps[0])
+            lastLapsTimeString.append(lapTimerTimestamps[0])
+            let firstLapTime = Int(lapTimerTimestamps[0].replacingOccurrences(of: "[:|,]", with: "", options: .regularExpression)) ?? 0
+            lapTimes.append(firstLapTime)
+        } else {
+            let countOfArray = lapTimerTimestamps.count
+            let strTimestampLastLap = lapTimerTimestamps[countOfArray - 1]
+            let strTimestampPreLastLap = lapTimerTimestamps[countOfArray - 2]
+            print(strTimestampLastLap)
+            print(strTimestampPreLastLap)
+
+            let timestampLastLap = Int(strTimestampLastLap.replacingOccurrences(of: "[:|,]", with: "", options: .regularExpression)) ?? 0
+            let timestampPreLastLap = Int(strTimestampPreLastLap.replacingOccurrences(of: "[:|,]", with: "", options: .regularExpression)) ?? 0
+
+            let lapTime = timestampLastLap - timestampPreLastLap
+            lapTimes.append(lapTime)
+            print(lapTime)
+
+            let lapTimeString = formatTime(timerPeriod: lapTime)
+
+            lastLapsTimeString.insert(lapTimeString, at: 0)
+        }
+    }
+    
     func timerDelegate(tuple : (minutes: Int, seconds: Int, fractions: Int)) {
         timerLabel.text = "\(String(format: "%02d", tuple.minutes)):\(String(format: "%02d", tuple.seconds)),\(String(format: "%02d", tuple.fractions))"
     }
     
     
     // MARK: - UITableViewDataSource
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return array.count
+        return lapCount
     }
-    //      MARK: - Как сделать dequeueReusableCell?
+    
+    //      MARK: - dequeueReusableCell?
     //        tableView.dequeueReusableCell(withIdentifier: "lapCell", for: indexPath)
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        tableView.separatorStyle = .singleLine
-        tableView.separatorInset = .init(top: 0, left: 10, bottom: 0, right: 10)
-        tableView.separatorColor = .white
-        tableView.backgroundColor = .black
         let cell = UITableViewCell(style: UITableViewCell.CellStyle.value1, reuseIdentifier: "lapCell")
-        let number = array[indexPath.row]
+        cell.backgroundColor = .black
         
-//      MARK: - Деприкейтед - исправить
+        //      MARK: - Depricated figure out
         //        var content = cell.defaultContentConfiguration()
         //        content.text = number
         //        content.secondaryText = number
         //        cell.contentConfiguration = content
-        cell.backgroundColor = .black
-        cell.textLabel?.text = number
-        cell.detailTextLabel?.text = "1"
-        cell.textLabel?.textColor = .white
-        cell.detailTextLabel?.textColor = .white
+        
+        let lapTableNumber = lapCount - indexPath.row
+        let lastLapTimeString = lastLapsTimeString[indexPath.row]
+        cell.textLabel?.text = "Lap " + String(lapTableNumber)
+        if lapCount < 3 {
+            cell.textLabel?.textColor = .white
+            cell.detailTextLabel?.textColor = .white
+            cell.detailTextLabel?.text = String(lastLapTimeString)
+        } else {
+            let maxMeasureValue = lapTimes.max()
+            let minMeasureValue = lapTimes.min()
+            let strMaxMeasureValue = String(maxMeasureValue!)
+            let strMinMeasureValue = String(minMeasureValue!)
+
+            let cellText = lastLapTimeString
+
+            cell.detailTextLabel?.text = cellText
+            let cellTextWithoutPunktMarks = cellText.replacingOccurrences(of: "[:|,]", with: "", options: .regularExpression)
+
+            if cellTextWithoutPunktMarks.contains(strMaxMeasureValue) {
+                cell.detailTextLabel?.textColor = .red
+                cell.textLabel?.textColor = .red
+            }
+            else if cellTextWithoutPunktMarks.contains(strMinMeasureValue) {
+                cell.detailTextLabel?.textColor = .green
+                cell.textLabel?.textColor = .green
+            }
+            else {
+                cell.detailTextLabel?.textColor = .white
+                cell.textLabel?.textColor = .white
+            }
+        }
         return cell
     }
     
